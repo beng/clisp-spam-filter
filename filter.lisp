f493d081ac3811c658b48c03ccbcb29bd62fd8a9
Ben Gelb <beg5670@gmail.com>
2012-07-07 17:43:58 -0400

    added class to store word-features

--- filter.lisp
+++ filter.lisp
@@ -11,3 +11,20 @@
     ((<= score *max-ham-score*) 'ham)
     ((>= score *min-spam-score*) 'spam)
     (t 'unsure)))
+
+(defclass word-feature ()
+  ((word
+    :initarg :word
+    :accessor word
+    :initform (error "must supply :word")
+    :documentation "the word this file represents.")
+   (spam-count
+    :initarg :spam-count
+    :accessor spam-count
+    :initform 0
+    :documentation "number of spams we have seen this feature in.")
+   (ham-count
+    :initarg :ham-count
+    :accessor ham-count
+    :initform 0
+    :documentation "number of hams we have seen this feature in.")))
